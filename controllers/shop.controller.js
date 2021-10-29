948eae398b54899726b016c8adc1400973250371
D4v1dD4art3 <jesusdavidduartejimenez@gmail.com>
2021-10-28 21:13:19 -0400

    🚀⚙️ purge a bit the workspace 🚀⚙️

--- controllers/shop.controller.js
+++ controllers/shop.controller.js
@@ -5,18 +5,14 @@ exports.getCart = (req, res, next) => {
   req.user
     .getCart()
     .then((cart) => {
-      return cart
-        .getProducts()
-        .then((products) => {
-          res.render('shop/cart', {
-            path: '/cart',
-            docTitle: 'Your Cart',
-            products: products,
-          });
-        })
-        .catch((err) => {
-          console.log(err);
-        });
+      return cart.getProducts();
+    })
+    .then((products) => {
+      res.render('shop/cart', {
+        path: '/cart',
+        docTitle: 'Your Cart',
+        products: products,
+      });
     })
     .catch((err) => {
       console.log(err);
