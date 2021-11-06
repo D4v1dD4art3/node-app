31719d06c33f6eddcd124c851f5ed7b54db43d80
D4v1dD4art3 <jesusdavidduartejimenez@gmail.com>
2021-11-06 12:38:17 -0400

    mongodb connection process

--- app.js
+++ app.js
@@ -1,18 +1,12 @@
 const express = require('express');
 const path = require('path');
-const sequelize = require('./utils/database');
-const Product = require('./models/product.model');
-const User = require('./models/user.model');
-const Cart = require('./models/cart.model');
-const CartItem = require('./models/cart-item.model');
-const Order = require('./models/order.model');
-const OrderItem = require('./models/order-items.model');
+const mongoDbConnect = require('./utils/database');
 const app = express();
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-const adminRoutes = require('./routes/admin.route');
-const shopRoutes = require('./routes/shop.route');
+// const adminRoutes = require('./routes/admin.route');
+// const shopRoutes = require('./routes/shop.route');
 const errorController = require('./controllers/error.controller');
 
 app.use(express.urlencoded({ extended: true }));
@@ -24,48 +18,23 @@ app.use((req, res, next) => {
 });
 
 app.use((req, res, next) => {
-  User.findByPk(1)
-    .then((user) => {
-      req.user = user;
-      next();
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+  // User.findByPk(1)
+  //   .then((user) => {
+  //     req.user = user;
+  //     next();
+  //   })
+  //   .catch((err) => {
+  //     console.log(err);
+  //   });
 });
 
-app.use('/admin', adminRoutes);
-app.use(shopRoutes);
+// app.use('/admin', adminRoutes);
+// app.use(shopRoutes);
 app.use(errorController.get404);
 
-Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
-User.hasMany(Product);
-User.hasOne(Cart);
-Cart.belongsTo(User);
-Cart.belongsToMany(Product, { through: CartItem });
-Product.belongsToMany(Cart, { through: CartItem });
-Order.belongsTo(User);
-User.hasMany(Order);
-Order.belongsToMany(Product, { through: OrderItem });
-sequelize
-  // .sync({ force: true })
-  .sync()
-  .then((result) => {
-    return User.findByPk(1);
-    // console.log(result);
-  })
-  .then((user) => {
-    if (!user) {
-      return User.create({ name: 'David', email: 'david@test.com' });
-    }
-    return user;
-  })
-  .then((user) => {
-    return user.createCart();
-  })
-  .then((cart) => {
-    app.listen(3000);
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+mongoDbConnect((clientObject) => {
+  console.log(clientObject);
+  app.listen(3000);
+});
+
+// SjNV2JvCEynwUCAQ mondoDb password
--- utils/database.js
+++ utils/database.js
@@ -1,8 +1,17 @@
-const { Sequelize } = require('sequelize');
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
 
-const sequelize = new Sequelize('node-complete', 'root', 'v25798979D', {
-  dialect: 'mysql',
-  host: 'localhost',
-});
+const mongoConnect = (callback) => {
+  MongoClient.connect(
+    'mongodb+srv://v25798979D:SjNV2JvCEynwUCAQ@cluster0.e7urz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+  )
+    .then((result) => {
+      console.log('Connected!');
+      callback(result);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
 
-module.exports = sequelize;
+module.exports = mongoConnect;
